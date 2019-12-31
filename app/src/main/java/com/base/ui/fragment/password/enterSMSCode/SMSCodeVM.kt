dd482953a03bcff2d98ee57f80a2b5387e807a36
Mete Ev Desktop <metesoydas@gmail.com>
2019-12-31 12:32:54 +0300

    Removed unnecessary UI files from sample code.

--- app/src/main/java/com/base/ui/fragment/password/enterSMSCode/SMSCodeVM.kt
+++
@@ -1,9 +0,0 @@
-package com.base.ui.fragment.password.enterSMSCode
-
-import android.app.Application
-import com.base.base.BaseViewModel
-import com.base.network.APIClient
-
-class SMSCodeVM(app: Application, private val client: APIClient) : BaseViewModel(app, client) {
-
-}
